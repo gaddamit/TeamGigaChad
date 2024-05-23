49d9d3b40c2a27f802a0635630921c31e21c26ac
Leo Esguerra <leo.esguerra@gmail.com>
2024-05-23 05:09:30 -0600

    Adjust buttons for webGL builds

--- 01_OneButton/Assets/MainMenu.cs
+++ 01_OneButton/Assets/MainMenu.cs
@@ -2,12 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+
+    public void Awake()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        #if UNITY_WEBGL
+        playButton.gameObject.transform.localPosition = new Vector3(0, -400, 0);
+        quitButton.gameObject.SetActive(false);
+        #endif
+    }
+
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(0);
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
@@ -15,5 +30,4 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
         Debug.Log("Game Quit");
     }
-
 }

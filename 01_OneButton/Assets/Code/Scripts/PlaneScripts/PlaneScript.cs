8b4824242b3c0fa22ed77127b9b0f0ee90a66e2b
Nomad Mason <xr3thx@gmail.com>
2024-05-30 11:58:17 -0600

    plane now moves
    not sure how we want to implement this

--- 01_OneButton/Assets/Code/Scripts/PlaneScripts/PlaneScript.cs
+++ 01_OneButton/Assets/Code/Scripts/PlaneScripts/PlaneScript.cs
@@ -1,6 +1,8 @@
+using JetBrains.Rider.Unity.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class PlaneScript : MonoBehaviour
 {
@@ -8,14 +10,24 @@ public class PlaneScript : MonoBehaviour
     private Vector3 loc;
     [SerializeField] private GameObject Gun;
     [SerializeField] private GameObject projectile;
-    [SerializeField] private Rigidbody2D projectileRB;
+    private GameObject burst1;
+    private GameObject burst2;
+    private GameObject burst3;
+
+    public Rigidbody2D planeRB;
+    private Rigidbody2D burst1RB;
+    private Rigidbody2D burst2RB;
+    private Rigidbody2D burst3RB;
+
     [SerializeField] private int thrust;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        projectileRB = projectile.GetComponent<Rigidbody2D>();
+        planeRB = GetComponent<Rigidbody2D>();
+        planeRB.gravityScale = 0;
+        planeRB.drag = 1;
     }
 
     // Update is called once per frame
@@ -26,25 +38,41 @@ public class PlaneScript : MonoBehaviour
 
            
                     StartCoroutine(gunBurstDelay());
-                
-                
-            
+         
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            planeRB.AddForce(this.transform.right * -thrust *3 );
 
         }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            planeRB.AddForce(this.transform.right * thrust *3 );
+
+        }
+
 
     }
     IEnumerator gunBurstDelay()
     {
-        Instantiate(projectile, FireLocation.transform.position, FireLocation.transform.rotation);
-        projectileRB.AddForce(transform.up * thrust, ForceMode2D.Impulse);
+        burst1 = Instantiate(projectile, FireLocation.transform.position, FireLocation.transform.rotation);
+        burst1RB = burst1.GetComponent<Rigidbody2D>();
+        burst1RB.AddForce(transform.right * thrust, ForceMode2D.Impulse);
+        burst1RB.AddForce(transform.up * -thrust, ForceMode2D.Impulse);
+
 
         yield return new WaitForSeconds(.1f);
-        Instantiate(projectile, FireLocation.transform.position, FireLocation.transform.rotation);
-        projectileRB.AddForce(FireLocation.transform.forward * 500);
+        burst2 = Instantiate(projectile, FireLocation.transform.position, FireLocation.transform.rotation);
+        burst2RB = burst2.GetComponent<Rigidbody2D>();
+        burst2RB.AddForce(transform.right * thrust, ForceMode2D.Impulse);
+        burst2RB.AddForce(transform.up * -thrust, ForceMode2D.Impulse);
+
 
         yield return new WaitForSeconds(0.1f);
-        Instantiate(projectile, FireLocation.transform.position, FireLocation.transform.rotation);
-        projectileRB.AddForce(FireLocation.transform.forward * 500);
+        burst3 = Instantiate(projectile, FireLocation.transform.position, FireLocation.transform.rotation);
+        burst3RB = burst3.GetComponent<Rigidbody2D>();
+        burst3RB.AddForce(transform.right * thrust, ForceMode2D.Impulse);
+        burst3RB.AddForce(transform.up * -thrust, ForceMode2D.Impulse);
 
         yield return null;
     }

1b81a26e89752616f4fe320566a22dfbc80b7a31
Leo Esguerra <leo.esguerra@gmail.com>
2024-07-17 07:26:34 -0600

    Added pellets and pacman

---
+++ 05_PacMan2/Assets/Scripts/Collectable.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player collected a collectable");
+            Destroy(gameObject);
+        }
+    }
+}

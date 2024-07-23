87285299ab1c1988c4efbefa6ba4cc318c0e00a8
Leo Esguerra <leo.esguerra@gmail.com>
2024-07-23 13:48:31 -0600

    Fix singleton issues when switching scenes

--- 05_PacMan2/Assets/Scripts/Singletons/GameManager.cs
+++ 05_PacMan2/Assets/Scripts/Singletons/GameManager.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.Events;
 using TMPro;
 
 public class GameManager : Singleton<GameManager>
@@ -7,9 +8,10 @@ public class GameManager : Singleton<GameManager>
     private int _score = 0;
     [SerializeField]
     private int _scoreIntervals = 100;
-    public TMP_Text scoreText;
+    public TMP_Text _scoreText;
     public Player player;
 
+    public UnityEvent onGameOver;
     public override void Awake()
     {
         base.Awake();
@@ -23,17 +25,27 @@ public class GameManager : Singleton<GameManager>
 
     public void IncreaseScore(int score)
     {
+        if(_scoreText == null)
+        {
+            _scoreText = GameObject.Find("ScoreValue").GetComponent<TMP_Text>();
+        }
+
         _score += score;
-        scoreText.text = $"{_score}";
+        _scoreText.text = $"{_score}";
 
         if((_score % _scoreIntervals) == 0)
         {
+            if(player == null)
+            {
+                player = GameObject.Find("PacMan").GetComponent<Player>();
+            }
+
             player.IncreaseSpeed();
         }
     }
 
     public void GameOver()
     {
-        //UIManager.Instance.ShowGameOverScreen();
+        onGameOver.Invoke();
     }   
 }

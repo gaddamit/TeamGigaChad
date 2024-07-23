44927fb0302535d37b0592f9177dbed06f32f3a5
Leo Esguerra <leo.esguerra@gmail.com>
2024-07-23 10:22:04 -0600

    Fix pellets getting left behind by increasing follow speed

--- 05_PacMan2/Assets/Scripts/PowerUps/FollowTarget.cs
+++ 05_PacMan2/Assets/Scripts/PowerUps/FollowTarget.cs
@@ -4,14 +4,16 @@ using DG.Tweening;
 public class FollowTarget : MonoBehaviour
 {
     public Transform target;
-    private Tween _tween;
+    public float speed = 60.0f;
     public void Update()
     {
         if (target != null)
         {
             Vector3 targetPosition = target.position;
-            targetPosition.y += 1.0f;
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, 0.1f);
+            targetPosition.y += 2.0f;
+
+            float step =  speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
         }
     }
 }

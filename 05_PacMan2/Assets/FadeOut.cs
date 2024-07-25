5939117d95dafbceee8e7bb9f5d956a5795812c4
Leo Esguerra <leo.esguerra@gmail.com>
2024-07-25 12:28:31 -0600

    Blur vision mechanic for pink ghost

---
+++ 05_PacMan2/Assets/FadeOut.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class FadeOut : MonoBehaviour
+{
+    private Tween _fadeTween;
+    private Image _image;
+
+    private void Awake()
+    {
+        _image = GetComponent<Image>();
+        _image.enabled = false;
+    }
+
+    public void FadeOutObject(Color color, float duration)
+    {
+        if(_fadeTween != null)
+        {
+            _fadeTween.Kill();
+        }
+
+        _image.color = color;
+        _image.enabled = true;
+        _fadeTween = _image.DOFade(0, duration).OnComplete(() => {
+            _image.enabled = false;
+        });
+    }
+}

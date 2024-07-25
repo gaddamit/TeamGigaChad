3d4492003fee9cb9d7d4b0a2caff0feddfe0d2e2
Leo Esguerra <leo.esguerra@gmail.com>
2024-07-25 13:22:15 -0600

    Fix popup menus resize

--- 05_PacMan2/Assets/Scripts/Singletons/GameManager.cs
+++ 05_PacMan2/Assets/Scripts/Singletons/GameManager.cs
@@ -66,5 +66,8 @@ public class GameManager : Singleton<GameManager>
     {
         _diedMenu = GameObject.Find("PlayerDied").GetComponent<PauseMenu>();
         _diedMenu.Pause();
+
+        TMP_Text score = GameObject.Find("PlayerDied/Score").GetComponent<TMP_Text>();
+        score.text = _score.ToString();
     }   
 }

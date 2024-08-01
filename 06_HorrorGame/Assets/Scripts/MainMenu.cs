3e233b8f8a88e6de1f8afd614465e2b96645ffba
IamNomadic <xr3thx@gmail.com>
2024-08-01 12:28:20 -0600

    hete

---
+++ 06_HorrorGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+
+    public void Awake()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+#if UNITY_WEBGL
+        // Quit Button removed for WebGL build
+        quitButton.gameObject.SetActive(false);
+#endif
+    }
+
+    public void PlayGame()
+    {
+        Debug.Log("called");
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}

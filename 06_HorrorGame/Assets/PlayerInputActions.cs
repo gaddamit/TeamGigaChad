c22ebcb2a298c05d1ab4e0b113cc0c5f9b4f4912
IamNomadic <xr3thx@gmail.com>
2024-08-02 11:31:28 -0600

    pause bind and volume slider

---
+++ 06_HorrorGame/Assets/PlayerInputActions.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerInputActions : MonoBehaviour
+{
+    private InputActions playerInput;
+    public PauseMenu pauseMenu;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+
+        
+        if (playerInput == null)
+        {
+            playerInput = new InputActions();
+            playerInput.menu.pause.performed += i => pauseMenu.Pause();
+            
+
+        }
+
+        playerInput.Enable();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
